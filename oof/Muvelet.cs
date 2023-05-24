896c08cfca336729bbbf58fe803bb9f3bad5dbb6
sztom <sztom@TOMI0928>
2023-05-24 21:48:44 +0200

    Add project files.

---
+++ oof/Muvelet.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oof
+{
+    internal class Muvelet
+    {
+        int a, b;
+        string str;
+
+        public int A { get => a; set => a = value; }
+        public int B { get => b; set => b = value; }
+        public string STR { get => str; set => str = value; }
+
+        public string Result
+        {
+            get
+            {
+                try
+                {
+                    switch (STR)
+                    {
+                        case "mod":
+                            return (A % B).ToString();
+                        case "/":
+                            return (A * 1.00 / B).ToString();
+                        case "div":
+                            return (A / B).ToString();
+                        case "-":
+                            return (A - B).ToString();
+                        case "*":
+                            return (A * B).ToString();
+                        case "+":
+                            return (A + B).ToString();
+                        default:
+                            return "hiba!";
+                    }
+                }
+                catch (Exception)
+                {
+                    return "hiba!";
+                }
+            }
+        }
+
+        public Muvelet(string line)
+        {
+            string[] data = line.Split(' ');
+            A = int.Parse(data[0].Trim());
+            str = data[1].Trim();
+            B = int.Parse(data[2].Trim());
+        }
+
+        public override string ToString()
+        {
+            return $"{A} {STR} {B} = {Result}";
+        }
+    }
+}
---
+++ oof/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace oof
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Muvelet> list = new List<Muvelet>();
+            using (StreamReader f = new StreamReader("kifejezesek.txt"))
+            {
+                while (!f.EndOfStream)
+                {
+                    list.Add(new Muvelet(f.ReadLine()));
+                }
+            }
+            Console.WriteLine($"Kifejezések Száma: {list.Count}");
+            Console.WriteLine($"Maradékos Osztásssal: {list.Count(x => x.STR == "mod")}");
+
+            int index = 0;
+            bool found = false;
+            do
+            {
+                found = list[index].A % 10 == 0 && list[index].B % 10 == 0;
+                index++;
+            } while (!found);
+            Console.WriteLine($"{(found ? "Van" : "Nincs")} ilyen kifejezés!");
+            Console.WriteLine("Statisztika");
+            list.Where(x => x.STR == "+" || x.STR == "-" || x.STR == "*" || x.STR == "/" || x.STR == "div" || x.STR == "mod").GroupBy(x => x.STR).ToList().ForEach(y => Console.WriteLine($"\t{y.Key} -> {y.Count()} db"));
+            string input = "";
+            do
+            {
+                if (input != "")
+                {
+                    try
+                    {
+                        Muvelet muvelet = new Muvelet(input);
+                        Console.WriteLine($"\t{muvelet.ToString()}");
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("\tHibás muvelet!");
+                    }
+                }
+                Console.Write("Adj meg egy muveletet (pl: 7 * 7): ");
+                input = Console.ReadLine();
+            } while (input != "");
+
+            using (StreamWriter f = new StreamWriter("eredmenyek.txt"))
+            {
+                list.ForEach(x => f.WriteLine(x.ToString()));
+            }
+            Console.WriteLine("eredmenyek.txt");
+            Console.ReadKey();
+        }
+    }
+}
